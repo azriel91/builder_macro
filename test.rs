6ca7452415a9f5a45a30c601484929fa112a39b5
Azriel Hoh <azriel91@gmail.com>
2016-09-28 18:17:44 +1300

    Added ability to specify fields with docs

--- test.rs
+++ test.rs
@@ -17,7 +17,7 @@ macro_rules! declare_struct_and_builder {
         $( #[$META] )*
         struct $STRUCT {
             $(
-                $( #[$META] )*
+                $( #[$F_META] )*
                 $F_NAME : $F_TY,
             )*
         }
@@ -40,21 +40,60 @@ macro_rules! parse_item {
     =>
     {
         parse_item! {
-            meta: [ #[$NEXT_META] $( #[$ITEM_META] )* ],
+            meta: [ $( #[$ITEM_META] )* #[$NEXT_META] ],
             spec: $( $SPEC )+
         }
     };
 
     // When we reach here, we have parsed all of the meta items for the struct.
-    // Next we have to parse the meta items for each field.
+    // Next we have to extract the tokens for each field into a block, then parse the meta items for each field.
+    // We have to do this because the rust compiler does not allow us to use a macro within the struct body:
+    //
+    // struct Something {
+    //     parse_fields!( $( $FIELD_SPEC )* ); // compilation failure
+    // }
+    //
+    // It also does not allow us to call a macro as part of another macro:
+    //
+    // a_macro!($something, parse_item!($another_thing)); // fails because it doesn't attempt to evaluate parse_item!
+    //
+
+    // This macro adds 'fields:' around the block to make parsing easier
     (
         meta: [ $( #[$ITEM_META:meta] )* ],
         spec: $BUILDER:ident => $STRUCT:ident {
+            $( $FIELD_SPEC:tt )*
+        }
+    )
+    =>
+    {
+        parse_item! {
+            meta: [ $( #[$ITEM_META] )* ],
+            spec: $BUILDER => $STRUCT,
+            fields: {},
+            field_wip: { meta: [] },
+            parser_wip: { $( $FIELD_SPEC )* }
+        }
+    };
+
+    // Now we have to attempt to wrap each field inside braces {}
+    // This macro looks for meta tokens and extracts them into field_wip
+    (
+        meta: [ $( #[$ITEM_META:meta] )* ],
+        spec: $BUILDER:ident => $STRUCT:ident,
+        fields: {
             $(
-                $FIELD_SPEC:tt
-                $(, $FIELDS_SPEC_ADDITIONAL:tt )*
-                $(,)*
+                {
+                    meta: [ $( #[$FIELD_META:meta] )* ],
+                    spec: $( $FIELD_SPEC:tt )+
+                },
             )*
+        },
+        field_wip: {
+            meta: [ $( #[$FIELD_WIP_META:meta] )* ]
+        },
+        parser_wip: {
+            #[$FIELD_WIP_NEXT_META:meta] $( $FIELD_TAIL:tt )+
         }
     )
     =>
@@ -65,21 +104,22 @@ macro_rules! parse_item {
             fields: {
                 $(
                     {
-                        meta: [],
-                        spec: $FIELD_SPEC
-                    }
-                    $(,
-                        {
-                            meta: [],
-                            spec: $( $FIELDS_SPEC_ADDITIONAL )*
-                        }
-                    )*
+                        meta: [ $( #[$FIELD_META] )* ],
+                        spec: $( $FIELD_SPEC )+
+                    },
                 )*
+            },
+            field_wip: {
+                meta: [ $( #[$FIELD_WIP_META] )* #[$FIELD_WIP_NEXT_META] ]
+            },
+            parser_wip: {
+                $( $FIELD_TAIL )+
             }
         }
     };
 
-    // Loop through each meta item in FIELD_SPEC, extract it and prepend it to FIELD_META
+    // When we reach here, the meta tokens for field_wip should have all been parsed
+    // Therefore we should be able to match on the field_name: Type = Some(default), pattern
     (
         meta: [ $( #[$ITEM_META:meta] )* ],
         spec: $BUILDER:ident => $STRUCT:ident,
@@ -87,26 +127,40 @@ macro_rules! parse_item {
             $(
                 {
                     meta: [ $( #[$FIELD_META:meta] )* ],
-                    spec: #[$NEXT_FIELD_META:meta] $( $FIELD_SPEC:tt )*
-                } $(,)*
+                    spec: $( $FIELD_SPEC:tt )+
+                },
             )*
+        },
+        field_wip: {
+            meta: [ $( #[$FIELD_WIP_META:meta] )* ]
+        },
+        parser_wip: {
+            $F_NAME:ident: $F_TY:ty = $F_DEFAULT:expr,
+            $( $FIELD_TAIL:tt )*
         }
     )
     =>
     {
-        yay
-        // parse_item! {
-        //     meta: [ $( #[$ITEM_META] )* ],
-        //     spec: $BUILDER => $STRUCT,
-        //     fields: {
-        //         $(
-        //             {
-        //                 meta: [ #[$NEXT_FIELD_META] $( #[$FIELD_META] )* ],
-        //                 spec: $( $FIELD_SPEC )*
-        //             },
-        //         )*
-        //     }
-        // }
+        parse_item! {
+            meta: [ $( #[$ITEM_META] )* ],
+            spec: $BUILDER => $STRUCT,
+            fields: {
+                $(
+                    {
+                        meta: [ $( #[$FIELD_META] )* ],
+                        spec: $( $FIELD_SPEC )+
+                    },
+                )*
+                {
+                    meta: [ $( #[$FIELD_WIP_META] )* ],
+                    spec: $F_NAME: $F_TY = $F_DEFAULT
+                },
+            },
+            field_wip: { meta: [] },
+            parser_wip: {
+                $( $FIELD_TAIL )*
+            }
+        }
     };
 
     (
@@ -119,7 +173,9 @@ macro_rules! parse_item {
                     spec: $F_NAME:ident: $F_TY:ty = $F_DEFAULT:expr $(,)*
                 } $(,)*
             )*
-        }
+        },
+        field_wip: { meta: [] },
+        parser_wip: {}
     )
     =>
     {
@@ -163,15 +219,29 @@ fn main() {
 
     builder! {
         TwoBuilder => Two {
-            something: i32 = Some(0)
+            something: i32 = Some(0),
         }
     }
 
-    // builder! {
-    //     /// hello everyone
-    //     MyStructBuilder => MyStruct {
-    //         /// doc for i32
-    //         something: i32 = Some(0)
-    //     }
-    // }
+    builder! {
+        ThreeBuilder => Three {
+            /// here are some docs
+            something: i32 = Some(0),
+        }
+    }
+
+    builder! {
+        FourBuilder => Four {
+            something: i32 = Some(0),
+            something_else: &'static str = Some("0"),
+        }
+    }
+
+    builder! {
+        /// hello everyone
+        MyStructBuilder => MyStruct {
+            /// doc for i32
+            something: i32 = Some(0),
+        }
+    }
 }
